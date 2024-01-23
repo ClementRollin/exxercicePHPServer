2410d2857323859afe78bd4fcaa1d02a3fb6b7c5
ClementRollin <clemrollin01@gmail.com>
2024-01-23 14:11:21 +0100

    mise à jour du code

--- index.php
+++ index.php
@@ -1,32 +1,54 @@
 <?php
-function getMessage() {
+
+
+
+function getMessage()
+{
     $message = "Visiteur";
     $messageVisiteur = null;
 
-    if (!isset($GET['name']) && !empty($_GET['name'])) {
-        $message = htmlspecialchars($_GET['name']);
+    if(isset($_GET['name']) && !empty($_GET['name'])){
+        $messageVisiteur = htmlspecialchars($_GET['name']);
     }
+
+    if($messageVisiteur){
+        $message = $messageVisiteur;
+    }
+
 
     return $message;
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>test</title>
-    </head>
-    <body>
-        <h1>Bonjour Clément, je suis connecté</h1>
-        <h4>Bonjour <?= getMessage() ?></h4>
 
-        <form action="index.php">
-            <div>
-                <label for="name">
-                    <input type="text" name="name" placeholder="Nom"/>
-                    <button type="submit">Sign In</button>
-                </label>
-            </div>
-        </form>
-    </body>
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Projet Gaetan</title>
+
+
+</head>
+<body>
+
+<h1>Bonjour je suis le projet gaetan</h1>
+<p>si vous voyez cette page c'est que je suis en ligne</p>
+
+<h4>Bonjour <?= getMessage() ?></h4>
+
+
+
+<form action="index.php" method="get">
+    <div><input type="text" name="name" id="" placeholder="name"></div>
+    <div>
+        <button type="submit">OK</button>
+    </div>
+</form>
+
+</body>
 </html>
